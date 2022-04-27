07a9aa7dbf76701a32fb01f9ba5ab1c8e1af6b1d
Rich <git.ramsey@gmail.com>
2022-04-27 13:24:44 +1000

    tests if RStudio can be used to push successfully

--- experiment-1/analysis/plan_n.R
+++ experiment-1/analysis/plan_n.R
@@ -1,3 +1,5 @@
 # Prospective power analysis
 
 # This is just a test to see how scripts are updated in git and how older versions are accessed
+
+# This tests if new changes can be pushed from RStudio successfully
